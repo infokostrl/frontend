b05ad9539d2475ae613ede4c6d8b4b0af20293a5
dandygardad <dandygardad@gmail.com>
2021-07-17 17:32:39 +0800

    Update style.css, disable login modal and add search page

--- js/script.js
+++ js/script.js
@@ -1,21 +1,17 @@
 // Take body document
-const body = document.body;
-// Login
-const login_open = document.getElementById("login_box");
-const login_closed = document.getElementById("login_closed");
-const login = document.getElementById('login')
+// const body = document.body;
+// // Login
+// const login_open = document.getElementById("login_box");
+// const login_closed = document.getElementById("login_closed");
+// const login = document.getElementById('login')
 
 // Login open close event
-login_open.addEventListener('click', function(){
-    login.classList.add('show');
-    body.classList.add('show');
-});
+// login_open.addEventListener('click', function(){
+//     login.classList.add('show');
+//     body.classList.add('show');
+// });
 
-login_closed.addEventListener('click', function(){
-    login.classList.remove('show');
-    body.classList.remove('show');
-});
-
-$(function () {
-    $('[data-toggle="popover"]').popover()
-})
+// login_closed.addEventListener('click', function(){
+//     login.classList.remove('show');
+//     body.classList.remove('show');
+// });
